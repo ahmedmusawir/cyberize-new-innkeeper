6b8b3bd27ce5b32aab4b655f146e52eda1400317
imbdev <moose@inspiredmommyboss.com>
2019-11-28 14:30:36 +0800

    Home Page template on going - 28 Nov 2019

--- template-home-page.php
+++ template-home-page.php
@@ -34,117 +34,105 @@ get_header(); ?>
     $inn_query = new WP_Query( $args );
   ?>
 
-    <div class="container">
+  <div class="container">
 
-    
+    <div class="row">
 
-      <div class="row">
+      <?php
+        if($inn_query->have_posts()) : 
+          while($inn_query->have_posts()) : 
+              $inn_query->the_post(); 
+      
+              $post_id = get_the_ID();
+              $featured_img_url = get_the_post_thumbnail_url($post_id,'full');
+      ?>
+      <div class="col-md-4">        
+          <div class="featured-box">
+          <div class="top-img-box" style="background: url(<?php echo $featured_img_url; ?>) no-repeat; background-size: cover;">
+            <a href="<?php the_permalink(); ?>">
+              <div class="featured-image-box">
+                <!-- THE COVER ON TOP OF THE BG TO LINK TO PERMALINK -->
+              </div>
+            </a>
+          </div>
+          <div class="middle-content-box">
 
-                <?php
-                  if($inn_query->have_posts()) : 
-                    while($inn_query->have_posts()) : 
-                       $inn_query->the_post(); 
-                
-                       $post_id = get_the_ID();
-                       $featured_img_url = get_the_post_thumbnail_url($post_id,'full');
-                ?>
-                <div class="col-md-4">        
-                
-                <div class="col-sm-12 col-md-12 col-lg-12 left-col" style="background: url(<?php echo $featured_img_url; ?>) no-repeat; background-size: cover;">
-                    <a href="<?php the_permalink(); ?>">
-                      <div class="featured-image-box">
-                        <!-- THE COVER ON TOP OF THE BG TO LINK TO PERMALINK -->
-                      </div>
-                      <div class="price-box">
-                        <h6><?php the_field('listing_member_status'); ?></h6>
-                        <h3 class="the-price">
-                          <!-- $99-$1200 -->
-                          <?php the_field('listing_item_price'); ?>
-                        </h3>    
-                      </div>
-                    </a>
-                  </div>
-                  <div class="col-sm-12 col-md-12 col-lg-12 right-col">
-
-                    <!-- TITLE -->
-                    <?php
-
-                    if ( is_singular() ) :
-                      the_title( '<h1 class="entry-title">', '</h1>' );
-                    else :
-                      the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-                    endif;
-
-                    ?>  
-                    <div class="entry-meta">
-                      <section class="info-box row">
-                        <article class="left-box col-6">
-                          <div class="row">
-                            <div class="col-3">
-                              <i class="fas fa-map-marked-alt"></i>
-                            </div>
-                            <div class="col-9">
-                              <!-- 500 Palm Ave Boca Grande, FL 33921 -->
-                              <?php the_field('listing_business_address'); ?>
-                            </div>
-                          </div>
-                        </article>
-                        <article class="right-box col-6">
-                          <div class="row">
-                            <div class="col-3">
-                              <i class="fas fa-phone-volume"></i>
-                            </div>
-                            <div class="col-9">
-                              <?php the_field('listing_phone_1'); ?> <br>
-                              <?php the_field('listing_phone_2'); ?> 
-                            </div>
-                          </div>
-                        </article>
-                      </section>
-                    </div><!-- .entry-meta -->
-
-                    <div class="the-excerpt pr-3">
-                      <?php
-                      // <!-- CONTENT -->
-                        the_excerpt();
-                        
-                      ?>
-                      <div class="bottom-btn-box text-center">
-                        <section class="info-box row">
-                          <article class="left-btn-box col-6">
-                            <a class="btn btn-primary" href="<?php the_field('listing_visit_website'); ?>" target="_blank">Visit Website</a>
-                          </article>
-                          <article class="right-btn-box col-6">
-                            <a class="btn btn-secondary" href="<?php the_permalink(); ?>">More Info</a>
-                          </article>
-                        </section>
-                      </div><!-- .entry-meta -->
-
-                      <!-- Change Ownership Ends -->
-                      
-                    </div>
-
-                  </div>
-
-                </div>
-                <?php
-                      endwhile;
-                  else : 
-                ?>
-
-                      Oops, there are no posts.
-
-                <?php
-                  endif;
-                ?>
-
+            <!-- TITLE -->
+            <?php
           
-				
+              the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+
+            ?>  
+            <div class="entry-meta">
+              <section class="info-box row">
+                <article class="col-6">
+                  <div class="row">
+                    <div class="col-3">
+                      <i class="fas fa-map-marked-alt"></i>
+                    </div>
+                    <div class="col-9 text">
+                      <!-- 500 Palm Ave Boca Grande, FL 33921 -->
+                      <?php the_field('listing_business_address'); ?>
+                    </div>
+                  </div>
+                </article>
+                <article class="col-6">
+                  <div class="row">
+                    <div class="col-3">
+                      <i class="fas fa-phone-volume"></i>
+                    </div>
+                    <div class="col-9 text">
+                      <?php the_field('listing_phone_1'); ?> <br>
+                      <?php the_field('listing_phone_2'); ?> 
+                    </div>
+                  </div>
+                </article>
+              </section>
+            </div><!-- .entry-meta -->
+
+            <div class="the-excerpt pr-3">
+              
+              <div class="btn-box text-center">
+                <section class="info-box row">
+                  <article class="left-btn-box col-6">
+                    <a class="btn btn-primary" href="<?php the_field('listing_visit_website'); ?>" target="_blank">Visit Website</a>
+                  </article>
+                  <article class="right-btn-box col-6">
+                    <a class="btn btn-secondary" href="<?php the_permalink(); ?>">More Info</a>
+                  </article>
+                </section>
+              </div><!-- .entry-meta -->
+
+              <!-- Change Ownership Ends -->
+              
+            </div>
+
+          </div>
+          <div class="bottom-pricebox text-center">
+            <div class="price-box">
+              <h6><?php the_field('listing_member_status'); ?></h6>
+              <h3 class="the-price">
+                <!-- $99-$1200 -->
+                <?php the_field('listing_item_price'); ?>
+              </h3>    
+            </div>
+          </div>
+          </div>
       </div>
+      <?php
+            endwhile;
+        else : 
+      ?>
 
+            Oops, there are no posts.
 
-
+      <?php
+        endif;
+      ?>
+      
     </div>
+
+  </div>
 
   </section> <!-- End section-two -->	
 
